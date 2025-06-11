762a6f372c9942a0af0bd62f90bf5569b6f8677b
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-06-12 00:28:29 +0900

    commit

--- studyplus/routes.py
+++ studyplus/routes.py
@@ -3,3 +3,9 @@ from handler.auth_handler import get_auth
 
 r = APIRouter()
 r.add_api_route("/auth", get_auth, methods=["POST"])
+
+# sqliteに登録される
+r.add_api_route("/weekly-record", lambda: None, methods=["POST"])
+
+# 想定: mcp経由で学習記録を登録出来る
+r.add_api_route("/register-record/mcp", lambda: None, methods=["POST"])
