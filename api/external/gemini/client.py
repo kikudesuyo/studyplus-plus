046affde9e5d05d48e312511734f9b2b674a1d36
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-06-21 03:08:56 +0900

    勉強バトル用のプロンプト作成

--- api/external/gemini/client.py
+++ api/external/gemini/client.py
@@ -19,12 +19,3 @@ def generate_content_with_gemini(
     if not response.text:
         raise ValueError("No text returned from Gemini API")
     return response.text
-
-
-if __name__ == "__main__":
-    prompt = """
-    あなたはプロの漫才師です。以下のエピソードが100点満点で何点かを評価してください。点数だけを出力してください。
-    イヤホンを1時間くらい探していたら、イヤホンをしていた。
-    """
-    result = generate_content_with_gemini(prompt)
-    print(f"Gemini response: {result}")
--- api/service/study_weekly_battle.py
+++ api/service/study_weekly_battle.py
@@ -3,6 +3,7 @@ from typing import List
 
 from pydantic import BaseModel
 
+from api.external.gemini.client import generate_content_with_gemini
 from api.external.studyplus.study_records import StudyRecord
 from api.external.studyplus.timeline_feeds import BodyStudyRecord, TimelineFeeds
 from api.model.user_model import UserModel, UserPlaceModel
@@ -166,7 +167,38 @@ def register_weekly_study_battle(start: datetime, end: datetime):
         end=end,
         user_places=user_places,
     )
-    # study_record.post(battle_material_code, 0, comment=comment)
+    study_record.post(battle_material_code, 0, comment=comment)
+
+
+def generate_study_battle_sammary_by_gemini(user_places: List[UserPlaceModel]) -> str:
+    prompt = f"""
+    あなたは勉強時間バトルの結果を要約し、感情的でポジティブなトーンでコメントを生成してください。
+    出力は「はい、わかりました」や「以下が結果です」などの前置きや説明を一切含めず、直接コメント本文のみを出力してください。
+    
+    ユーザー名:miyavin 
+    一人称は「miyavin」を使用してください。
+    使用する場合は、いずれかの一人称を選び、統一してください。
+
+    勉強時間バトルの結果は、ユーザー名、順位、勉強時間を含むデータです。
+    以下のデータは、1週間の勉強時間のランキングです。
+    データ:
+    {user_places}
+    ユーザー名、順位、勉強時間をもとに、以下の条件に従ってコメントを生成してください。
+    条件:
+    1. コメントは日本語で書いてください。
+    2. 結果を基に要約してください。
+    3. 勝者を中心に、他のユーザーの努力も称える内容にしてください。
+    4. コメントは感情的で、ポジティブなトーンで書いてください。
+    5. あなたが勉強をするわけではないので「見習って、私も頑張る」といった表現は使用しないでください。あくまでも他者を称える内容にしてください。
+    6. 他者に比べて圧倒的に勉強時間が優れている場合は、その点を強調して褒め称えてください。
+    7. 他者に比べて圧倒的に勉強時間が劣っている場合は、少しだけ煽るような内容にしてください。ユーモアのある煽りをするとより良いです。    
+    8. 最後に次回の勉強時間バトルへの期待感を表現してください。
+    7. 各ユーザーのコメントは1~2文程度にしてください。
+    8. 前置きや説明文は一切書かず、コメント本文のみを出力してください。
+    """
+    return generate_content_with_gemini(
+        prompt=prompt,
+    )
 
 
 def generate_weekly_battle_comment(
@@ -189,6 +221,8 @@ def generate_weekly_battle_comment(
         f"📣勉強時間バトル {start.strftime('%Y-%m-%d')} ~ {end.strftime('%Y-%m-%d')} の結果です📣\n\n"
         + f"{place_comment}\n\n"
         + f"勝者は {sorted_user_places[0].user.name} さんです！おめでとうございます🎉\n"
+        + f"miyavinからのコメントです:\n"
+        + generate_study_battle_sammary_by_gemini(sorted_user_places)
         + f"次回も頑張りましょう！🔥\n"
         + f"See you next week ;D"
     )
