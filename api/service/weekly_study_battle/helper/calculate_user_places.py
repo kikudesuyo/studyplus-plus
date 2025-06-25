2f6fa7ba17c8279b7d495e7443b6eab2ebdbc9e7
Ikuya Nagaki <ikuya313198@gmail.com>
2025-06-25 14:07:56 +0900

    refactor: calculate_user_places

--- api/service/weekly_study_battle/helper/calculate_user_places.py
+++ api/service/weekly_study_battle/helper/calculate_user_places.py
@@ -1,3 +1,4 @@
+from itertools import groupby
 from typing import List
 
 from api.service.weekly_study_battle.model import PlaceModel, TotalStudyDurationModel
@@ -11,29 +12,20 @@ def calculate_user_places(
     同じ学習時間の場合は同じ順位を割り当てる（例: 1位, 2位, 2位, 4位）。
     """
     sorted_user_durations = sorted(
-        user_total_study_duration,
-        key=lambda x: x.total_duration,
-        reverse=True,
+        user_total_study_duration, key=lambda x: x.total_duration, reverse=True
     )
 
     user_places: List[PlaceModel] = []
-    last_duration = -1
-    last_place = 0
-    for i, duration_model in enumerate(sorted_user_durations):
-        current_place: int
-        if duration_model.total_duration == last_duration:
-            current_place = last_place
-        else:
-            current_place = i + 1
-
-        user_places.append(
-            PlaceModel(
-                user=duration_model.user,
-                place=current_place,
-                total_duration=duration_model.total_duration,
+    current_place = 1
+    for _, group in groupby(sorted_user_durations, key=lambda x: x.total_duration):
+        group_list = list(group)
+        for duration_model in group_list:
+            user_places.append(
+                PlaceModel(
+                    user=duration_model.user,
+                    place=current_place,
+                    total_duration=duration_model.total_duration,
+                )
             )
-        )
-        last_duration = duration_model.total_duration
-        last_place = current_place
-
+        current_place += len(group_list)
     return user_places
