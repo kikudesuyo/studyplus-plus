e0c6082f543c1f146370ff28139481483e7675e5
Hiromu Kikuchi <111168523+kikudesuyo@users.noreply.github.com>
2025-07-09 01:41:30 +0900

    Update api/handler/bot_like_followees_records_handler.py
    Co-authored-by: Ikuya Nagaki <66161167+eteRnaL198@users.noreply.github.com>

--- api/handler/bot_like_followees_records_handler.py
+++ api/handler/bot_like_followees_records_handler.py
@@ -5,8 +5,6 @@ def handle_like_followees_records_by_bot():
     """
     フォロー中のユーザーの学習記録にいいねをするハンドラー
 
-    Args:
-        access_token: アクセストークン
     """
     access_token = "a5317c96-c5bd-4366-843f-a2068112ad95"
     like_followees_timeline_records_occasionally(access_token)
